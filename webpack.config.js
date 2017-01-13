75cf60e476ece73f43152bdcde98a0246d3c59a2
shinxi <xijx@primeton.com>
2017-01-13 15:21:01 +0800

    Update: Fix path issue.

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/build'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
